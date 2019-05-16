c658b4a179c5550bdf51725f631c8e0a4b7a4635
Killian COUTY <killian.couty@etu.univ-nantes.fr>
2019-05-16 10:41:01 +0200

    Point d'entrée /connexion

--- api.js
+++ api.js
@@ -8,7 +8,7 @@
 // Expressjs
 const express = require('express');
 // Notre module nodejs d'accès simplifié à la base de données
-const dbHelper = require('./dbhelper.js');
+//const dbHelper = require('./dbhelper.js');
 
 // Comme c'est un module nodejs il faut exporter les fonction qu'on veut rendre publiques
 // ici on n'exporte qu'ne seule fonction (anonyme) qui est le "constructeur" du module
@@ -16,5 +16,28 @@ const dbHelper = require('./dbhelper.js');
 module.exports = (passport) => {
     const app = express();
 
+    // Point d'entrée pour la connexion
+    app.get('/connexion', function (req, res, next) {
+        // @TODO : Vérifer le nom de la variable username
+        if (!req.body.username)
+            return res.send({success: false, message: 'empty username'});
+        // @TODO : Vérifer le nom de la variable password
+        if (!req.body.password)
+            return res.send({success: false, message: 'empty password'});
 
-}
+        passport.authenticate('local', function (err, user) {
+            if (err)
+                return next(err);
+            if (!user)
+                return res.redirect('/connexion');
+            req.login(user, function (err) {
+                if (err)
+                    return next(err);
+                return res.redirect('/index.html');
+            });
+        })(req, res, next);
+    });
+
+    return app;
+
+};
