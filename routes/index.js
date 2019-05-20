49276e4e682796b291aea9bdeccb8262aec46e1a
Thomas PEUZIAT <thomas.peuziat@etu.univ-nantes.fr>
2019-05-20 18:12:06 +0200

    Render template login sans css ni rien

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/*
 router.get('/', function(req, res, next) {
     res.send({success: true, message: 'ok'});
 });
@@ -9,5 +10,6 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     res.send({succes: true, message: 'ok'});
 });
+*/
 
 module.exports = router;
