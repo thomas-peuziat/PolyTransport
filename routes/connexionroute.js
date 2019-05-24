1fda8d4eeee6e185bd6a7862bea033d12200d1ba
Killian COUTY <killian.couty@etu.univ-nantes.fr>
2019-05-24 09:49:13 +0200

    Redirection après login

--- routes/connexionroute.js
+++ routes/connexionroute.js
@@ -3,9 +3,8 @@ let router = express.Router();
 //const accueil = require('../public/js/conn_get');
 //const conn = require('../public/js/conn');
 
-router.post('/', async function(req, res, next) {
-    // req.body.mail
-    // res.sendFile('/public/views/login1.html');
+router.post('/', function(req, res, next) {
+
 });
 
 router.get('/', function(req, res, next) {
