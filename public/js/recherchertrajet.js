5969033f8e65568187b93174cd11ff5d7f6a9d3d
Thomas PEUZIAT <thomas.peuziat@etu.univ-nantes.fr>
2019-05-28 10:55:00 +0200

    Parsing heure (00:12 --> 0012)

--- public/js/recherchertrajet.js
+++ public/js/recherchertrajet.js
@@ -15,9 +15,11 @@ async function renderRechercherPage(context) {
         const lieu_depart = document.querySelector('#inputDeparture').value;
         const lieu_arrivee = document.querySelector('#inputArrival').value;
         //const date_depart = document.querySelector('#inputDate').value;
-        const heure_depart = document.querySelector('#inputHour').value;
 
-        document.location.href = '/trajet/liste-trajets' + '?lieu_depart=' + lieu_depart + '&lieu_arrivee=' + lieu_arrivee  + '&heure_depart=' + heure_depart;
+        const nonParsedHeure_depart = document.querySelector('#inputHour').value;
+        const numHeure_depart = parseInt(nonParsedHeure_depart[0] + nonParsedHeure_depart[1] + nonParsedHeure_depart[3] + nonParsedHeure_depart[4]);
+
+        document.location.href = '/trajet/liste-trajets' + '?lieu_depart=' + lieu_depart + '&lieu_arrivee=' + lieu_arrivee  + '&heure_depart=' + numHeure_depart;
     });
 }
 
